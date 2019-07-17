6517dc89f21526501802fec0b394cb75146fa4e4
Kyler Barber <kylerbarber820@yahoo.com>
2019-07-17 20:17:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,3 +5,12 @@ def countdown(seconds)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
